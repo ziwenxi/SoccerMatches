6d83928c46ebf715c41dc380f8d249b0f9735eb5
ziwenxi <ziwen.xi@yuanpin-inc.com>
2016-03-05 16:45:57 +0800

    creat project

---
+++ SoccerMatches/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SoccerMatches
+//
+//  Created by ziwen.xi on 16/3/5.
+//  Copyright © 2016年 Apress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SoccerMatches/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SoccerMatches
+//
+//  Created by ziwen.xi on 16/3/5.
+//  Copyright © 2016年 Apress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

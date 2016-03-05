271d518d9d42fe87f2fdb41f130d3d7fb8d58b9c
ziwenxi <ziwen.xi@yuanpin-inc.com>
2016-03-05 16:59:10 +0800

    cocoapods

---
+++ Podfile
@@ -0,0 +1,3 @@
+platform :ios,’8.0’
+pod 'Masonry', '~> 0.6.3'
+pod 'RDVTabBarController', '~> 1.1.9'
--- SoccerMatches/AppDelegate.m
+++ SoccerMatches/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -16,8 +17,13 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    self.window=[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    
+    self.viewController=[[ViewController alloc] initWithNibName:nil bundle:nil];
+    self.window.rootViewController=self.viewController;
+    [self.window makeKeyAndVisible];
     return YES;
+
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
